c917260874d3c599c41e72b2e47bde1628e7572a
yakshi9 <140039815+yakshi9@users.noreply.github.com>
2023-11-10 15:16:11 +0530

    Create script.js

---
+++ Task2/script.js
@@ -0,0 +1,9 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Here you can add code to handle form submission, like validation or sending the data.
+    // Example: 
+    // const name = document.getElementById("name").value;
+    // const email = document.getElementById("email").value;
+    // const message = document.getElementById("message").value;
+    // Then, you might use this data to send an email or store it in a database.
+});
